5c741332eb620b42cbb5aeab2decc9889e4169b7
Oraldo <72758131+oraldo-doci@users.noreply.github.com>
2023-08-05 02:30:17 +0200

    add api call

--- next-deploy/src/app/asynPage/page.tsx
+++ next-deploy/src/app/asynPage/page.tsx
@@ -1,4 +1,10 @@
-export const asyncPage = async () => {
-  const apiRes = await fetch("https://rickandmortyapi.com/api/character/108");
-  return <>{JSON.stringify(apiRes)}</>;
-};
+export default async function Page() {
+  // This request should be cached until manually invalidated.
+  // Similar to `getStaticProps`.
+  // `force-cache` is the default and can be omitted.
+  const staticData = await fetch(
+    `https://swapi.dev/api/planets/3/?format=json`
+  );
+
+  return <div>{JSON.stringify(staticData)}</div>;
+}
